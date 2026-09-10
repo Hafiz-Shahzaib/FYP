0495aac420fd9df62e03501555908382d138c8fc
Muhammad Shahzaib <shahzaibshafi92@gmail.com>
2026-09-10 22:02:36 +0500

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,8 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: "http://localhost:5173",
+  // origin: "http://localhost:5173",
+  origin: "https://fyp-o9zs.vercel.app/",
   credentials: true,
 }))
 
